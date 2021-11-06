f7ba9949f9629bfd9d65e51813f4472df19d9a65
Sergey Tyulenev <marleeeeeey@gmail.com>
2021-11-06 21:55:08 +0300

    add zero_variable_by_reference function

--- 2021-11-06 2138 Change variable on function/Change_variable/Change_variable.cpp
+++ 2021-11-06 2138 Change variable on function/Change_variable/Change_variable.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
-void zero_variable(int* var)
+void zero_variable_by_pointer(int* var)
 {
     *var = 0;
+}
+
+void zero_variable_by_reference(int & var)
+{
+    var = 1;
 }
 
 int main()
 {
     int a = 4;
     std::cout << a << std::endl;
-    zero_variable(&a);
+    zero_variable_by_pointer(&a);
+    std::cout << a << std::endl;
+    zero_variable_by_reference(a);
     std::cout << a << std::endl;
 
     return 0;
