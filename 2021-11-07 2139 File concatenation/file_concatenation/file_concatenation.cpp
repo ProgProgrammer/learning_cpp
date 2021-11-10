a6cc16a797c2cc12221f3fadb7aceef42da66184
CS <zubr.free@yandex.ru>
2021-11-10 11:06:56 +0300

    Add modify code and 4 new files

--- 2021-11-07 2139 File concatenation/file_concatenation/file_concatenation.cpp
+++ 2021-11-07 2139 File concatenation/file_concatenation/file_concatenation.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < argc; i++)
+    std::string output_file = "Write file.txt";
+    std::ofstream outFile;
+
+    outFile.open(output_file);
+
+    if (outFile.is_open())
     {
-        std::cout << argv[i] << std::endl;
+        std::string line;
+
+        std::ifstream inFile;
+
+        for (int i = 0; i < argc; i++)
+        {
+            if (i > 0)
+            {
+                std::string str = argv[i];
+                std::cout << typeid(str).name() << std::endl;
+                inFile.open(str);
+                if (inFile.is_open())
+                {
+                    while (std::getline(inFile, line))
+                    {
+                        outFile << line << std::endl;
+                    }
+                }
+                else
+                {
+                    std::cout << "Can't open '" << str << "' file.";
+                    return 2;
+                }
+                inFile.close();
+            }
+        }
     }
+    else
+    {
+        std::cout << "Can't open '" << output_file << "' file.";
+        return 1;
+    }
+
+    outFile.close();
     return 0;
 }
