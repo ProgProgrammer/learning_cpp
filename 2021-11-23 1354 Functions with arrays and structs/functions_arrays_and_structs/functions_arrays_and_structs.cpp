ff744b0f973193f7d493a4d3de3150213c96824f
CS <zubr.free@yandex.ru>
2021-11-23 14:09:54 +0300

    Add functions with arrays and structs

---
+++ 2021-11-23 1354 Functions with arrays and structs/functions_arrays_and_structs/functions_arrays_and_structs.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+
+const int SLEN = 30;
+
+struct student {
+    char fullname[SLEN];
+    char hobby[SLEN];
+    int ooplevel;
+};
+
+int getinfo(student pa[], int n)
+{
+    std::string str;
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        std::cout << i + 1 << " student." << std::endl;
+        std::cout << "Entered full name student: ";
+
+        if (std::getline(std::cin, str) && str.length() != 0)
+        {
+            strcpy_s(pa[i].fullname, 30, str.c_str());
+        }
+        else
+        {
+            break;
+        }
+
+        std::cout << "Entered hobby student: ";
+
+        if (std::getline(std::cin, str))
+        {
+            strcpy_s(pa[i].hobby, 30, str.c_str());
+        }
+
+        std::cout << "Entered oop level: ";
+
+        if (std::cin >> str)
+        {
+            pa[i].ooplevel = stoi(str);
+        }
+
+        std::cin.clear();
+        std::cin.ignore();
+    }
+
+    return i;
+}
+
+void display1(student st)
+{
+    std::cout << "Name student: " << st.fullname << std::endl;
+    std::cout << "Hobby: " << st.hobby << std::endl;
+    std::cout << "Oop level: " << st.ooplevel << std::endl;
+}
+
+void display2(const student* st)
+{
+    std::cout << "Name student: " << st->fullname << std::endl;
+    std::cout << "Hobby: " << st->hobby << std::endl;
+    std::cout << "Oop level: " << st->ooplevel << std::endl;
+}
+
+void display3(const student pa[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Name student: " << pa[i].fullname << std::endl;
+        std::cout << "Hobby: " << pa[i].hobby << std::endl;
+        std::cout << "Oop level: " << pa[i].ooplevel << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "Enter class size: ";
+    int class_size;
+    std::cin >> class_size;
+
+    while (std::cin.get() != '\n')
+    {
+        continue;
+    }
+
+    student* ptr_stu = new student[class_size];
+
+    int entered = getinfo(ptr_stu, class_size);
+
+    for (int i = 0; i < entered; i++)
+    {
+        display1(ptr_stu[i]);
+        display2(&ptr_stu[i]);
+    }
+
+    display3(ptr_stu, entered);
+
+    delete[] ptr_stu;
+
+    std::cout << "Done" << std::endl;
+
+    return 0;
+}
