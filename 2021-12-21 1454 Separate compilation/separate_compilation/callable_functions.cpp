c303aa7484b97afd582c9e50067114f28b68b690
CS <zubr.free@yandex.ru>
2021-12-10 14:54:59 +0300

    Add separate compilation

---
+++ 2021-12-21 1454 Separate compilation/separate_compilation/callable_functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "coordin.h"
+
+polar rect_to_polar(rect xypos)
+{
+    polar answer;
+
+    answer.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+    answer.angle = atan2(xypos.y, xypos.x);
+
+    return answer;
+}
+
+void show_polar(polar dapos)
+{
+    const double Rad_to_deg = 57.29577951;
+
+    std::cout << "distance = " << dapos.distance;
+    std::cout << ", angle = " << dapos.angle * Rad_to_deg;
+    std::cout << " degrees" << std::endl;
+}
