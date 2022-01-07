981a94b63b52027db71709d5f7b492fa0f721d2e
CS <zubr.free@yandex.ru>
2022-01-07 17:45:06 +0300

    Add code

---
+++ 2022-01-07 1744 Error chat/1/1.cpp
@@ -0,0 +1,185 @@
+#pragma comment(lib, "ws2_32.lib")  // для получения доступа к некоторым функциям
+#include <iostream>
+#include <string>
+#include <winsock2.h>  // библиотека для работы с сетью
+#include <ws2tcpip.h>
+
+#pragma warning(disable: 4996)  // в этом коде эта ошибка компиляции выходить не будет (устаревшая функция inet_addr())
+
+static SOCKET connections[100];
+int counter = 0;
+
+namespace message
+{
+    void sendm(SOCKET newConnection, char * msg, int msg_size)
+    {
+        send(newConnection, (char*)&msg_size, sizeof(int), NULL);  // функция для отправки данных
+        send(newConnection, msg, msg_size, NULL);                  // функция для отправки данных
+    }
+
+    void ClientHandler(int index)
+    {
+        int msg_size;
+
+        while (recv(connections[index], (char*)&msg_size, sizeof(int), NULL))  // функция для получения данных
+        {
+            char* msg = new char[msg_size + 1];
+            msg[msg_size] = '\0';
+
+            recv(connections[index], msg, msg_size, NULL);  // функция для получения данных
+
+            for (int i = 0; i < counter; i++) {
+                if (i == index) 
+                {
+                    continue;
+                }
+
+                sendm(connections[i], msg, msg_size + 1);
+            }
+
+            delete [] msg;
+        }
+    }
+
+    void closeAllSockets()
+    {
+        std::cout << "Connections:" << std::endl;
+
+        for (int i = 0; i < counter; i++)
+        {
+            if (connections[i] > 0)
+            {
+                std::cout << "1. " << connections[i] << std::endl;
+                closesocket(connections[i]);
+                std::cout << "2. " << connections[i] << std::endl;
+            }
+        }
+    }
+
+    void actions(int n)
+    {
+        std::string str;
+        int num;
+
+        while (std::getline(std::cin, str))
+        {
+            if (str == "cl_all_cons")
+            {
+                closeAllSockets();
+
+                std::cout << "All connections are closed." << std::endl;
+
+                str = "";
+            }
+            else if (str == "cl_con")
+            {
+                std::cout << "Enter the connection number: ";
+                std::cin >> num;
+
+                if (num > 0 && closesocket(connections[num - 1]) == 0)
+                {
+                    std::cout << "Connection #" << num << " is closed." << std::endl;
+
+                    str = "";
+                }
+                else
+                {
+                    std::cout << "There is no such connection." << std::endl;
+                }
+            }
+            else if (str == "close_app")
+            {
+                if (counter > 0)
+                {
+                    closeAllSockets();
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char * argv[] )
+{
+    // WSAStartup - функция, для загрузки библиотеки
+    WSAData wasData; // создание структуры
+
+    WORD DLLVersion = MAKEWORD(2, 1);  // запрашиваемая версия библиотеки winsock - нужна для того, чтобы загрузить библиотеку
+    int res = WSAStartup(DLLVersion, &wasData);  // загружаем библиотеку
+
+    if (res != 0)
+    {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;  // нужна для хранения интернет протоколов и адреса
+
+    int domain = AF_INET;  // семейство используемых протоколов. В данном случае, AF_INET.
+    //unsigned char buf[sizeof(struct in6_addr)];
+
+    //addr.sin_addr.s_addr = inet_pton(domain, "127.0.0.1", buf);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // sin_addr - структура SOCKADDR_IN, которая хранит ip-адрес
+    addr.sin_port = htons(1111);  // sin_port - хранит порт ижентификации программы поступающими данными
+    addr.sin_family = domain;  // sin_family - хранит семейство протоколов. Для интернет-протоколов указывается контанта AF_INET
+
+    SOCKET sListen = socket(domain, SOCK_STREAM, NULL);   // создание сокета и с названием sListen, которому присовоен результат
+                                                          // выполнения фунции socket с параметрами: AF_INET - семейство протоколов AF_INET
+                                                          // SOCK_STREAM - протокол, устанавливающий соединение (потоковый тип создаваемого сокета)
+                                                          // NULL - при таком значении выбор транспортного протокола происходит по умолчанию.
+                                                          // TCP - для потоковых сокетов, как здесь.
+    
+    int size_addr = sizeof(addr);
+
+    bind(sListen, (SOCKADDR*)&addr, size_addr);  // функция, для привязки адреса сокету.
+                                                 // sListen - предварительно
+                                                 // созданный сокет
+                                                 // (SOCKADDR*)&addr - указатель на структуру SOCKADDR
+                                                 // sf_addr - размер структуры SOCKADDR
+
+    listen(sListen, SOMAXCONN);  // listen() - функция ожидания подключений.
+                                 // sListen - сокет, по которому функция определит, по каком порту нужно начать прослушивание.
+                                 // SOMAXCONN - максимальное количество запросов, ожидающих обработки. По-умолчанию, равен 128.
+                                 // можно написать точное количество запросов, например 3. В этом случае встанут в очередь 3 запроса, а остальные получат ошибку
+
+    SOCKET newConnection;  // создан новый сокет
+
+    std::cout << "To close all connections, type the word \"cl_all_cons\" and \"Enter\" keys." << std::endl;
+    std::cout << "To close all connections, type the word \"cl_con\" and \"Enter\", and then enter the connection number." << std::endl;
+    std::cout << "To exit the application, type the word \"close_app\" and press the \"Enter\" key." << std::endl << std::endl;
+
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message::actions, (LPVOID)(0), NULL, NULL);
+
+    for (int i = 0; i < 100; i++)
+    {
+        newConnection = accept(sListen, (SOCKADDR*)&addr, &size_addr);  // извлечение запросов на соединение из очереди
+
+        if (newConnection == 0)
+        {
+            std::cout << "Error connection." << std::endl;
+        }
+        else
+        {
+            std::cout << "Client #" << i + 1 << " connected." << std::endl; 
+            
+            std::string msg = "Welcome to the chat! Press enter twice to start a dialogue.\n";
+            int msg_size = msg.size();
+            char* str = new char[msg_size + 1];
+            str[msg_size] = '\0';
+
+            strcpy(str, msg.c_str());
+
+            message::sendm(newConnection, str, msg_size + 1);
+
+            delete [] str;
+
+            connections[i] = newConnection;
+            counter++;
+
+            CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message::ClientHandler, (LPVOID)(i), NULL, NULL);
+        }
+    }
+
+    system("pause");
+
+    return 0;
+}
---
+++ 2022-01-07 1744 Error chat/Client2/Clinet2/Clinet2.cpp
@@ -0,0 +1,114 @@
+#pragma comment(lib, "ws2_32.lib")  // для получения доступа к некоторым функциям
+#include <iostream>
+#include <winsock2.h>  // библиотека для работы с сетью
+#include <string>
+
+#pragma warning(disable: 4996)  // в этом коде эта ошибка компиляции выходить не будет (устаревшая функция inet_addr())
+
+static SOCKET connection;
+
+namespace message
+{
+    struct sendmessage
+    {
+        std::string name;
+        std::string message;
+        std::string all_t;
+    };
+
+    void sendm(SOCKET newConnection, std::string str)
+    {
+        int msg_size = str.size();
+        send(newConnection, (char*)&msg_size, sizeof(int), NULL);
+        send(newConnection, str.c_str(), str.length(), NULL);
+    }
+
+    void ClientHandler()
+    {
+        int msg_size;
+        int count = -1;
+
+        while (recv(connection, (char*)&msg_size, sizeof(int), NULL))  // функция для получения данных
+        {
+            count++;
+
+            char* msg = new char[msg_size];
+            msg[msg_size - 1] = '\0';
+
+            recv(connection, msg, msg_size, NULL);  // функция для получения данных
+
+            std::cout << msg << std::endl;
+
+            delete[] msg;
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // WSAStartup - функция, для загрузки библиотеки
+
+    WSAData wasData;
+
+    WORD DLLVersion = MAKEWORD(2, 1);
+    int res = WSAStartup(DLLVersion, &wasData);
+
+    if (res != 0)
+    {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+
+    int domain = AF_INET;
+
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1111);
+    addr.sin_family = domain;
+
+    connection = socket(domain, SOCK_STREAM, NULL);
+
+    if (connect(connection, (SOCKADDR*)&addr, sizeof(addr)) != 0)  // connect() - функция для соединения с сервером, где
+                                                                   // connection - дескриптор сокета, возращенный функцией socket
+                                                                   // (SOCKADDR*)&addr - указатель на структуру "sockaddr", содержащую в себе
+                                                                   // адрес и порт удаленного узла с которым устанавливается соединение
+                                                                   // sizeof(addr) - сообщает функции размер структуры sockaddr
+    {
+        std::cout << "Error: failed connect to server." << std::endl;
+        WSACleanup();  // деинициализация библиотеки WINSOCK и освобождения используемых этим приложением ресурсов
+        std::cin.get();
+
+        return 1;
+    }
+
+    std::cout << "Connected." << std::endl;
+
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message::ClientHandler, NULL, NULL, NULL);
+
+    std::cin.get();
+
+    std::cout << "Enter your name: ";
+
+    message::sendmessage str_mes;
+
+    std::getline(std::cin, str_mes.name);
+
+    std::cout << "Start correspondence:" << std::endl;
+
+    while (std::getline(std::cin, str_mes.message))
+    {
+        str_mes.all_t = "\t" + str_mes.name + "\n";
+        str_mes.all_t = str_mes.all_t + "\t" + str_mes.message + "\n\a";
+
+        message::sendm(connection, str_mes.all_t);
+
+        std::cout << std::endl;
+
+        Sleep(10);
+    }
+
+    system("pause");
+
+    return 0;
+}
