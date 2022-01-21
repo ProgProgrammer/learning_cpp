829291c195feca087965884e9feb5c7f8b5d1745
CS <zubr.free@yandex.ru>
2022-01-21 13:35:09 +0300

    Refactoring code

--- 2022-01-07 1744 Error chat/1/1.cpp
+++ 2022-01-07 1744 Error chat/1/1.cpp
@@ -1,4 +1,4 @@
-#pragma comment(lib, "ws2_32.lib")  // для получения доступа к некоторым функциям
+#pragma comment(lib, "ws2_32.lib")  // для получения доступа к некоторым функциям (линковка)
 #include <iostream>
 #include <string>
 #include <winsock2.h>  // библиотека для работы с сетью
@@ -6,54 +6,55 @@
 
 #pragma warning(disable: 4996)  // в этом коде эта ошибка компиляции выходить не будет (устаревшая функция inet_addr())
 
-static HANDLE hThreadArray[100];
-static SOCKET connections[100];
-int counter = 0;
+static HANDLE CLIENT_THREADS[100];  // массив потоков
+static SOCKET CLIENT_CONNECTIONS[100];   // массив сокетов
+int CLIENT_COUNTER = 0;
 
-namespace message
+namespace message_namespace
 {
-    struct mess
+    struct ConnectionParams
     {
         SOCKET sListen;
         SOCKADDR_IN addr;
         int size_addr;
     };
 
-    void sendm(SOCKET newConnection, char * msg, int msg_size)
+    void sendm(SOCKET newConnection, const char * msg, int msg_size)
     {
-        send(newConnection, (char*)&msg_size, sizeof(int), NULL);  // функция для отправки данных
-        send(newConnection, msg, msg_size, NULL);                  // функция для отправки данных
+        send(newConnection, (char*)&msg_size, sizeof(int), NULL);  // функция для отправки данных (размер данных)
+        send(newConnection, msg, msg_size, NULL);                  // функция для отправки данных (само сообщение)
     }
 
-    void ClientHandler(int index)
+    void ClientHandler(int client_index)
     {
         int msg_size;
 
-        while (recv(connections[index], (char*)&msg_size, sizeof(int), NULL))  // функция для получения данных
+        while (recv(CLIENT_CONNECTIONS[client_index], (char*)&msg_size, sizeof(int), NULL))  // функция для получения данных
         {
             char* msg = new char[msg_size + 1];
             msg[msg_size] = '\0';
 
-            recv(connections[index], msg, msg_size, NULL);  // функция для получения данных
+            recv(CLIENT_CONNECTIONS[client_index], msg, msg_size, NULL);  // функция для получения данных
 
-            for (int i = 0; i < counter; i++) {
-                if (i == index) 
+            for (int i = 0; i < CLIENT_COUNTER; i++) 
+            {
+                if (i == client_index) 
                 {
                     continue;
                 }
 
-                sendm(connections[i], msg, msg_size + 1);
+                sendm(CLIENT_CONNECTIONS[i], msg, msg_size + 1);
             }
 
             delete [] msg;
         }
     }
 
-    void createConnections(mess * m1)
+    void createConnections(ConnectionParams & params)
     {
         SOCKET newConnection;  // создан новый сокет
 
-        while ((newConnection = accept(m1->sListen, (SOCKADDR*)&m1->addr, &m1->size_addr)) && counter < 100)
+        while ((newConnection = accept(params.sListen, (SOCKADDR*)&params.addr, &params.size_addr)) && CLIENT_COUNTER < 100)
         {
             if (newConnection == 0)
             {
@@ -61,44 +62,37 @@ namespace message
             }
             else
             {
-                std::cout << "Client #" << counter + 1 << " connected." << std::endl;
+                std::cout << "Client #" << CLIENT_COUNTER + 1 << " connected." << std::endl;
 
                 std::string msg = "Welcome to the chat! Press enter twice to start a dialogue.\n";
-                int msg_size = msg.size();
-                char* str = new char[msg_size + 1];
-                str[msg_size] = '\0';
 
-                strcpy(str, msg.c_str());
+                message_namespace::sendm(newConnection, msg.c_str(), msg.size() + 1);
 
-                message::sendm(newConnection, str, msg_size + 1);
+                CLIENT_CONNECTIONS[CLIENT_COUNTER] = newConnection;
 
-                delete[] str;
+                CLIENT_THREADS[CLIENT_COUNTER] = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message_namespace::ClientHandler, (LPVOID)(CLIENT_COUNTER), NULL, NULL);
 
-                connections[counter] = newConnection;
-
-                hThreadArray[counter] = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message::ClientHandler, (LPVOID)(counter), NULL, NULL);
-
-                counter++;
+                CLIENT_COUNTER++;
             }
         }
     }
 
     void closeAllSockets()
     {
-        for (int i = 0; i < counter; i++)
+        for (int i = 0; i < CLIENT_COUNTER; i++)
         {
-            if (connections[i] > 0 && closesocket(connections[i]) == 0)
+            if (CLIENT_CONNECTIONS[i] > 0 && closesocket(CLIENT_CONNECTIONS[i]) == 0)
             {
-                connections[i] = 0;
+                CLIENT_CONNECTIONS[i] = 0;
             }
         }
     }
 
     void closeHandles()
     {
-        for (int i = 0; i < counter; i++)
+        for (int i = 0; i < CLIENT_COUNTER; i++)
         {
-            CloseHandle(hThreadArray[i]);
+            CloseHandle(CLIENT_THREADS[i]);
         }
     }
 }
@@ -106,10 +100,10 @@ namespace message
 int main(int argc, char * argv[] )
 {
     // WSAStartup - функция, для загрузки библиотеки
-    WSAData wasData; // создание структуры
+    WSAData wsaData; // создание структуры
 
     WORD DLLVersion = MAKEWORD(2, 1);  // запрашиваемая версия библиотеки winsock - нужна для того, чтобы загрузить библиотеку
-    int res = WSAStartup(DLLVersion, &wasData);  // загружаем библиотеку
+    int res = WSAStartup(DLLVersion, &wsaData);  // загружаем библиотеку
 
     if (res != 0)
     {
@@ -127,7 +121,7 @@ int main(int argc, char * argv[] )
     addr.sin_port = htons(1111);  // sin_port - хранит порт ижентификации программы поступающими данными
     addr.sin_family = domain;  // sin_family - хранит семейство протоколов. Для интернет-протоколов указывается контанта AF_INET
 
-    SOCKET sListen = socket(domain, SOCK_STREAM, NULL);   // создание сокета и с названием sListen, которому присовоен результат
+    SOCKET sListen = socket(domain, SOCK_STREAM, NULL);   // создание сокета и с названием sListen, которому присвоен результат
                                                           // выполнения фунции socket с параметрами: AF_INET - семейство протоколов AF_INET
                                                           // SOCK_STREAM - протокол, устанавливающий соединение (потоковый тип создаваемого сокета)
                                                           // NULL - при таком значении выбор транспортного протокола происходит по умолчанию.
@@ -150,48 +144,48 @@ int main(int argc, char * argv[] )
     std::cout << "To close all connections, type the word \"cl_con\" and \"Enter\", and then enter the connection number." << std::endl;
     std::cout << "To exit the application, type the word \"close_app\" and press the \"Enter\" key." << std::endl << std::endl;
 
-    message::mess* m1 = new message::mess;
+    message_namespace::ConnectionParams params;
     
-    m1->sListen = sListen;
-    m1->addr = addr;
-    m1->size_addr = size_addr;
+    params.sListen = sListen;
+    params.addr = addr;
+    params.size_addr = size_addr;
 
-    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message::createConnections, m1, NULL, NULL);
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)message_namespace::createConnections, &params, NULL, NULL);
 
-    std::string str;
-    int num;
+    std::string command;
+    int connection_num;
 
-    while (std::getline(std::cin, str))
+    while (std::getline(std::cin, command))
     {
-        if (str == "cl_all_cons")
+        if (command == "cl_all_cons")
         {
-            message::closeAllSockets();
-            message::closeHandles();
+            message_namespace::closeAllSockets();
+            message_namespace::closeHandles();
 
-            counter = 0;
+            CLIENT_COUNTER = 0;
 
             std::cout << "All connections are closed." << std::endl;
         }
-        else if (str == "cl_con")
+        else if (command == "cl_con")
         {
             std::cout << "Enter the connection number: ";
-            std::cin >> num;
+            std::cin >> connection_num;
 
-            if (num > 0 && closesocket(connections[num - 1]) == 0)
+            if (connection_num > 0 && closesocket(CLIENT_CONNECTIONS[connection_num - 1]) == 0)
             {
-                CloseHandle(hThreadArray[num - 1]);
+                CloseHandle(CLIENT_THREADS[connection_num - 1]);
             }
             else
             {
                 std::cout << "There is no such connection." << std::endl;
             }
         }
-        else if (str == "close_app")
+        else if (command == "close_app")
         {
-            if (counter > 0)
+            if (CLIENT_COUNTER > 0)
             {
-                message::closeAllSockets();
-                message::closeHandles();
+                message_namespace::closeAllSockets();
+                message_namespace::closeHandles();
             }
 
             std::cout << "The program has ended." << std::endl;
