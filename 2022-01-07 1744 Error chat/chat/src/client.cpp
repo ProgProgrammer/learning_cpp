836792635f6c525d6e59afd269646f729be1531f
Sergey Tyulenev <marleeeeeey@gmail.com>
2022-01-23 00:08:15 +0300

    Solution chat refactoring: codepage and src file location

--- 2022-01-07 1744 Error chat/chat/src/client.cpp
+++ 2022-01-07 1744 Error chat/chat/src/client.cpp
@@ -1,9 +1,9 @@
-#pragma comment(lib, "ws2_32.lib")  // для получения доступа к некоторым функциям
+п»ї#pragma comment(lib, "ws2_32.lib")  // РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РґРѕСЃС‚СѓРїР° Рє РЅРµРєРѕС‚РѕСЂС‹Рј С„СѓРЅРєС†РёСЏРј
 #include <iostream>
-#include <winsock2.h>  // библиотека для работы с сетью
+#include <winsock2.h>  // Р±РёР±Р»РёРѕС‚РµРєР° РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ СЃРµС‚СЊСЋ
 #include <string>
 
-#pragma warning(disable: 4996)  // в этом коде эта ошибка компиляции выходить не будет (устаревшая функция inet_addr())
+#pragma warning(disable: 4996)  // РІ СЌС‚РѕРј РєРѕРґРµ СЌС‚Р° РѕС€РёР±РєР° РєРѕРјРїРёР»СЏС†РёРё РІС‹С…РѕРґРёС‚СЊ РЅРµ Р±СѓРґРµС‚ (СѓСЃС‚Р°СЂРµРІС€Р°СЏ С„СѓРЅРєС†РёСЏ inet_addr())
 
 static SOCKET CONNECTION;
 
@@ -28,14 +28,14 @@ namespace message_namespace
         int msg_size;
         int count = -1;
 
-        while (recv(CONNECTION, (char*)&msg_size, sizeof(int), NULL))  // функция для получения данных
+        while (recv(CONNECTION, (char*)&msg_size, sizeof(int), NULL))  // С„СѓРЅРєС†РёСЏ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РґР°РЅРЅС‹С…
         {
             count++;
 
             char* msg = new char[msg_size];
             msg[msg_size - 1] = '\0';
 
-            recv(CONNECTION, msg, msg_size, NULL);  // функция для получения данных
+            recv(CONNECTION, msg, msg_size, NULL);  // С„СѓРЅРєС†РёСЏ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ РґР°РЅРЅС‹С…
 
             std::cout << msg << std::endl;
 
@@ -46,7 +46,7 @@ namespace message_namespace
 
 int main(int argc, char* argv[])
 {
-    // WSAStartup - функция, для загрузки библиотеки
+    // WSAStartup - С„СѓРЅРєС†РёСЏ, РґР»СЏ Р·Р°РіСЂСѓР·РєРё Р±РёР±Р»РёРѕС‚РµРєРё
 
     WSAData wsaData;
 
@@ -69,15 +69,15 @@ int main(int argc, char* argv[])
 
     CONNECTION = socket(domain, SOCK_STREAM, NULL);
 
-    if (connect(CONNECTION, (SOCKADDR*)&addr, sizeof(addr)) != 0)  // connect() - функция для соединения с сервером, где
-                                                                   // connection - дескриптор сокета, возращенный функцией socket
-                                                                   // (SOCKADDR*)&addr - указатель на структуру "sockaddr", содержащую в себе
-                                                                   // адрес и порт удаленного узла с которым устанавливается соединение
-                                                                   // sizeof(addr) - сообщает функции размер структуры sockaddr
+    if (connect(CONNECTION, (SOCKADDR*)&addr, sizeof(addr)) != 0)  // connect() - С„СѓРЅРєС†РёСЏ РґР»СЏ СЃРѕРµРґРёРЅРµРЅРёСЏ СЃ СЃРµСЂРІРµСЂРѕРј, РіРґРµ
+                                                                   // connection - РґРµСЃРєСЂРёРїС‚РѕСЂ СЃРѕРєРµС‚Р°, РІРѕР·СЂР°С‰РµРЅРЅС‹Р№ С„СѓРЅРєС†РёРµР№ socket
+                                                                   // (SOCKADDR*)&addr - СѓРєР°Р·Р°С‚РµР»СЊ РЅР° СЃС‚СЂСѓРєС‚СѓСЂСѓ "sockaddr", СЃРѕРґРµСЂР¶Р°С‰СѓСЋ РІ СЃРµР±Рµ
+                                                                   // Р°РґСЂРµСЃ Рё РїРѕСЂС‚ СѓРґР°Р»РµРЅРЅРѕРіРѕ СѓР·Р»Р° СЃ РєРѕС‚РѕСЂС‹Рј СѓСЃС‚Р°РЅР°РІР»РёРІР°РµС‚СЃСЏ СЃРѕРµРґРёРЅРµРЅРёРµ
+                                                                   // sizeof(addr) - СЃРѕРѕР±С‰Р°РµС‚ С„СѓРЅРєС†РёРё СЂР°Р·РјРµСЂ СЃС‚СЂСѓРєС‚СѓСЂС‹ sockaddr
     {
         std::cout << "Error: failed connect to server." << std::endl;
-        closesocket(CONNECTION);  // удаление соединения
-        WSACleanup();  // деинициализация библиотеки WINSOCK и освобождения используемых этим приложением ресурсов
+        closesocket(CONNECTION);  // СѓРґР°Р»РµРЅРёРµ СЃРѕРµРґРёРЅРµРЅРёСЏ
+        WSACleanup();  // РґРµРёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ Р±РёР±Р»РёРѕС‚РµРєРё WINSOCK Рё РѕСЃРІРѕР±РѕР¶РґРµРЅРёСЏ РёСЃРїРѕР»СЊР·СѓРµРјС‹С… СЌС‚РёРј РїСЂРёР»РѕР¶РµРЅРёРµРј СЂРµСЃСѓСЂСЃРѕРІ
         std::cin.get();
 
         return 1;
