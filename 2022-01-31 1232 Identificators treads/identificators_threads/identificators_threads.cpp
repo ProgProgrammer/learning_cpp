4739217ded0362ccf910a803f26649041244a4de
CS <zubr.free@yandex.ru>
2022-01-31 12:34:00 +0300

    Add identifiators threads

---
+++ 2022-01-31 1232 Identificators treads/identificators_threads/identificators_threads.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+template <class T>
+void output(T i)
+{
+    std::cout << i + 1 << ") " << std::this_thread::get_id << std::endl;
+}
+
+int main()
+{
+    unsigned long const hardware_threads = std::thread::hardware_concurrency();  // функция std::thread::hardware_concurrency() возвращает количество аппаратных потоков
+
+    if (hardware_threads != 0)
+    {
+        std::cout << hardware_threads << std::endl;  // 12
+
+        std::vector<std::thread> threads(hardware_threads);  // создание массива для потоков в динамической памяти
+
+        for (int i = 0; i < hardware_threads; i++)
+        {
+            threads[i] = std::thread(output, i);
+
+            if (threads[i].joinable())
+            {
+                threads[i].join();
+            }
+        }
+    }
+    else
+    {
+        std::vector<std::thread> threads(2);
+
+        for (int i = 0; i < 2; i++)
+        {
+            threads[i] = std::thread(output, i);
+
+            if (threads[i].joinable())
+            {
+                threads[i].join();
+            }
+        }
+    }
+
+    return 0;
+}
