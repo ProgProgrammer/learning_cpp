bde64fc092191ed51837ebae57cad6349969e5f3
ProgProgrammer <progr.programmer@yandex.ru>
2023-03-25 01:06:40 +0300

    Added Task1 file in Laba3 project

---
+++ 2023-03-18 2122 Parallel programming/Laba3/Laba3/Task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "mpi.h"
+
+int main(int argc, char** argv)
+{
+	int rank, size;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	MPI_Finalize();
+
+	printf("Process: %d, size: %d\n", rank, size);
+
+	return 0;
+}
