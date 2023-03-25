f16b6c1a5476bab6701dbfe483fec54c67bf4858
ProgProgrammer <progr.programmer@yandex.ru>
2023-03-25 23:43:42 +0300

    Refactoring code in twoTask in Laba3 project

--- 2023-03-18 2122 Parallel programming/Laba3/Laba3/class/parallel_two_task.cpp
+++ 2023-03-18 2122 Parallel programming/Laba3/Laba3/class/parallel_two_task.cpp
@@ -4,7 +4,10 @@
 void Parallel::twoTask(const int argc, char** argv)
 {
 	int nError, nProvided, nRank;
-	nError = MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &nProvided);
+	//nError = MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &nProvided);
+	//nError = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &nProvided);
+	//nError = MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &nProvided);
+	nError = MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &nProvided);
 
 	if (nError != MPI_SUCCESS)
 	{
@@ -12,7 +15,10 @@ void Parallel::twoTask(const int argc, char** argv)
 		return;
 	}
 	MPI_Comm_rank(MPI_COMM_WORLD, &nRank);
-	if (nProvided < MPI_THREAD_SERIALIZED)
+	//if (nProvided < MPI_THREAD_SERIALIZED)
+	//if (nProvided < MPI_THREAD_MULTIPLE)
+	//if (nProvided < MPI_THREAD_FUNNELED)
+	if (nProvided < MPI_THREAD_SINGLE)
 	{
 		if (nRank == 0)
 			std::cout << "Serialized MPI not supportde, provided " << nProvided
@@ -34,7 +40,10 @@ void Parallel::twoTask(const int argc, char** argv)
 			int nThread = omp_get_thread_num();
 			MPI_Is_thread_main(&bMain);
 			cMain = (bMain ? main : n_main);
-			std::cout << "MPI_THREAD_SERIALIZED: Process, " << nRank << " Thread " << nThread	<< cMain << std::endl;
+			//std::cout << "MPI_THREAD_SERIALIZED: Process, " << nRank << " Thread " << nThread	<< cMain << std::endl;
+			//std::cout << "MPI_THREAD_MULTIPLE Process, " << nRank << " Thread " << nThread	<< cMain << std::endl;
+			//std::cout << "MPI_THREAD_FUNNELED Process, " << nRank << " Thread " << nThread	<< cMain << std::endl;
+			std::cout << "MPI_THREAD_SINGLE Process, " << nRank << " Thread " << nThread	<< cMain << std::endl;
 		}
 	}
 
